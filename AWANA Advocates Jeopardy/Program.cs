cb9ec838c570dd2975c70a8bc427547c5eb65781
samson206 <samsonl206@gmail.com>
2022-10-11 19:44:10 -0400

    test

--- AWANA Advocates Jeopardy/Program.cs
+++ AWANA Advocates Jeopardy/Program.cs
@@ -8,9 +8,6 @@ namespace AWANA_Advocates_Jeopardy
 {
     static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
